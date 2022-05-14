7f6183479ea23c44cb2452fbe7b36de778d626f7
jcjen62 <jcjensen62@gmail.com>
2022-05-14 14:38:25 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Gallery from "./Gallery.svelte";
+</script>
+
+<main>
+	<h1>Haley & Jeremy Jensen</h1>
+	<Gallery />
+</main>
+
+<style scoped>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #315d6b;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ src/Gallery.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { fade, fly } from 'svelte/transition';
+	let visible = false;
+
+    const handleClick = () => {
+        visible = true
+    }
+</script>
+
+<label>
+	<button on:click={handleClick}>
+	    See Photos
+    </button>
+</label>
+
+{#if visible}
+    <img in:fly="{{ y: 200, duration: 2000 }}" out:fade src="./_M7A5145.jpg" alt="Test"/>
+{/if}
+
+<style>
+    img {
+        width: 100%;
+        height: auto;
+    }
+
+    button {
+        padding: 12px 10px;
+        border: none;
+        background-color: rgb(157, 174, 189);
+        color: white;
+    }
+</style>
