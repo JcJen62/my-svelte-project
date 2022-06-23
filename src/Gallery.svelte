2b065c73595652b87ef824e93e94655a6c56ecd6
jcjen62 <jcjensen62@gmail.com>
2022-06-22 21:08:16 -0600

    test

--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -1,27 +1,33 @@
 <script>
-	import { fade, fly } from 'svelte/transition';
-    import Gallery from 'svelte-image-gallery'
-	let visible = false;
+    import { fade, fly } from "svelte/transition";
+    import Gallery from "svelte-image-gallery";
+    let visible = false;
+    let name = 'Show'
 
     const handleClick = () => {
-        visible = true
-    }
+        visible = !visible;
+        if (visible === true){
+            name = 'Hide'
+        } else if (visible === false){
+            name = 'Show'
+        }
+    };
 </script>
 
 <label>
-	<button on:click={handleClick}>
-	    See Photos
-    </button>
+    <button on:click={handleClick}> {name} Photos </button>
 </label>
 
 {#if visible}
-    <Gallery gap="10" maxColumnWidth='200'>
-        <img in:fly="{{ y: 200, duration: 2000 }}" out:fade src="./_M7A5145.jpg" alt="Test"/>
-
+    
+    <Gallery gap="10" maxColumnWidth="200">
+        <img
+            in:fly={{ y: 200, duration: 2000 }}
+            out:fade
+            src="./_M7A5145.jpg"
+            alt="Test"
+        />
     </Gallery>
-    <div class="container">
-        
-    </div>
 {/if}
 
 <style>
